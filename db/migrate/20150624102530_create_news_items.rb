5cb75757b6bf205c23a95178d278cb300d5b3424
severynsky <severynr@gmail.com>
2015-06-24 13:46:49 +0300

    added news-item and category models

---
+++ db/migrate/20150624102530_create_news_items.rb
@@ -0,0 +1,11 @@
+class CreateNewsItems < ActiveRecord::Migration
+  def change
+    create_table :news_items do |t|
+      t.string :title
+      t.text :intro_text
+      t.text :body
+      t.integer :category_id
+      t.timestamps null: false
+    end
+  end
+end
