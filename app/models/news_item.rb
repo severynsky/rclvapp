8fb6522a40f0bc24aa0839228ef4537c268f73f3
severynsky <severynr@gmail.com>
2015-06-24 18:19:55 +0300

    added image model and setted as polymorphic

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,3 +1,8 @@
 class NewsItem < ActiveRecord::Base
+  
   belongs_to :category
+  has_one :image, as: :imageable, dependent: :destroy
+
+  accepts_nested_attributes_for :image, :reject_if => lambda {|a| a[:picture].blank?}, allow_destroy: true
+  
 end
