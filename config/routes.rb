d96db354a2864aff49777e5f3d868c823cc1f40d
severynsky <severynr@gmail.com>
2015-06-25 10:55:52 +0300

    installed active admin added existing resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :newsitems
     resources :categories
+    resources :images
 
   # Example resource route with options:
   #   resources :products do
