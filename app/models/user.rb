3c98f9dd0748d5d409ca15a177d718d53c28da05
severynsky <severynr@gmail.com>
2015-07-03 10:40:02 +0300

    user model disable register ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-  devise :timeoutable, :timeout_in => 1.minutes
+  devise :timeoutable, :timeout_in => 15.minutes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
