2f2b293181018f0a115cf6ebff0b2ed600dfdadf
severynsky <severynr@gmail.com>
2015-07-09 11:55:17 +0300

    fix after member devise problems

---
+++ app/views/members/show.json.jbuilder
@@ -0,0 +1,4 @@
+json.id @member.id
+json.name @member.name
+json.role @member.role
+json.address @member.addres
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707114704) do
+ActiveRecord::Schema.define(version: 20150709065809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,9 +102,21 @@ ActiveRecord::Schema.define(version: 20150707114704) do
     t.string   "languages"
     t.string   "nationality"
     t.date     "date_of_birth"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "members", ["email"], name: "index_members_on_email", unique: true, using: :btree
+  add_index "members", ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
 
   create_table "news_items", force: :cascade do |t|
     t.string   "title"
