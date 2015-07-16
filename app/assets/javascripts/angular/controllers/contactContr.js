c2c768ce9ed34a014b1d5ad13be67f8cb4743421
severynsky <severynr@gmail.com>
2015-07-16 17:50:52 +0300

    added fb widget, login lang styling other minor fixes

--- app/assets/javascripts/angular/controllers/contactContr.js
+++ app/assets/javascripts/angular/controllers/contactContr.js
@@ -1,4 +1,13 @@
 rclvapp.controller('contactContr', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
     $scope.pageClass = "contactContr";
     
+      FB = null;
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        // if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v2.4&appId=591757474261298";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    
 }]);
