eb1fef235d14b3ef3755669e47a4de5ad157e913
severynsky <severynr@gmail.com>
2018-06-12 11:42:12 +0300

    caching on users list, styling fixing

--- app/assets/javascripts/angular/controllers/usersListContr.js
+++ app/assets/javascripts/angular/controllers/usersListContr.js
@@ -1,28 +1,36 @@
 'use strict'
 
-rclvapp.controller('usersListContr', ['$scope', '$http', 'Auth', 'getAllUsers', 'getUsers', '$timeout', function($scope, $http, Auth, getAllUsers, getUsers, $timeout){
+rclvapp.controller('usersListContr', ['$scope', '$rootScope', '$http', 'Auth', 'getAllUsers', 'getUsers', '$timeout', function($scope, $rootScope, $http, Auth, getAllUsers, getUsers, $timeout){
     $scope.pageClass = "usersList";
     $scope.loadingData = true;
     $scope.dataLoaded = false;
+    var users;
+    $scope.users;
+
+    console.log('users: ', $rootScope.userList);
+
+    if (!$rootScope.userList) {
+      Auth.currentUser()
+        .then(function(user) {
+          $scope.users = getAllUsers.query();
+          handleData($scope.users);
+        }, function(error){
+          $scope.errorMessage = error.data.error;
+          $scope.users = getUsers.query();
+          handleData($scope.users);
+        });
+    } else {
+      $scope.loadingData = false;
+      $scope.dataLoaded = true;
+    }
 
     var handleData = function(source) {
       source.$promise.then(function (result) {
         $scope.loadingData = false;
         $scope.dataLoaded = true;
-        $scope.users = result;
+        $rootScope.userList = result;
       });
     }
-
-    Auth.currentUser()
-      .then(function(user) {
-        $scope.users = getAllUsers.query();
-        handleData($scope.users);
-      }, function(error){
-        $scope.errorMessage = error.data.error;
-        $scope.users = getUsers.query();
-        handleData($scope.users);
-      });
-
 
     // $scope.toggleInfo = function(){
     //   debugger;
